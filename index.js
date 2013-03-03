2bdbec5b26c525beeb6a0f966e673c7743a88a5e
David Smith <codeactual@gmail.com>
2013-03-03 01:28:54 -0800

    Refactor indent, etc

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
 /*jshint node:true*/
 "use strict";
 
-var each = require('each'),
-    toString = Object.prototype.toString,
-    types = ['Arguments', 'Function', 'String', 'Number', 'Date', 'RegExp', 'Array'];
+var each = require('each');
+var types = ['Arguments', 'Function', 'String', 'Number', 'Date', 'RegExp', 'Array'];
 
 each(types, function (type) {
-    var method = type === 'Function' ? type : type.toLowerCase();
-    module.exports[method] = function (obj) {
-        return toString.call(obj) === '[object ' + type + ']';
-    };
+  var method = type === 'Function' ? type : type.toLowerCase();
+  module.exports[method] = function (obj) {
+    return Object.prototype.toString.call(obj) === '[object ' + type + ']';
+  };
 });
 
 if (Array.isArray) {
-    module.exports.array = Array.isArray;
+  module.exports.array = Array.isArray;
 }
 
 module.exports.object = function (obj) {
-    return obj === Object(obj);
+  return obj === Object(obj);
 };
 
